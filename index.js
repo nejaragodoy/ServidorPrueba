0c96d52a3f16b6ee7c833e447d7ab94b987d9c98
Nehuen <nejaragodoy@gmail.com>
2021-10-28 21:14:38 -0300

    Fin tutorial Express

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const morgan = require('morgan')
+
 const app = express();
 
-app.use(express.json());
+function logger(req, res, next){
+	console.log(`Ruta recibida ${req.protocol} ://${req.get('host')}${req.originalUrl}`);
+	next();
+}
+//Settings
+app.set('appName', 'Nehuen');
+app.set('port', 3000);
+app.set('view engine', 'ejs');
+//Middlewares
+app.use(express.json()); 
+app.use(morgan('dev'));
+
+
+//app.all();
+
+app.get('/', (req, res)=>{
+	const data = [{name:'nehuen'},{name:'nehuen2'},{name:'nehuen3'}]
+	res.render('index.ejs', {people: data});
+})
 
 app.get('/user', (req, res)=>{
 	res.json({
@@ -15,6 +35,13 @@ app.post('/user/:id', (req, res)=>{
 	console.log('POST REQUEST RECIVED');
 })
 
-app.listen(3000, () =>{
-	console.log('Server Port 3001');
+app.delete('/user/:id', (req, res)=>{
+	console.log('User '+req.params.id+'deleted');
+	
+})
+
+app.use(express.static('public/'));
+
+app.listen(app.get('port'), () =>{
+	console.log('Server Port', app.get('port'));
 })
